d18a6d493e261c2fc8d9d910c6da433d9aa74610
MauricioLF <mauriciolf00@gmail.com>
2023-04-12 10:38:38 -0300

    Commit inicial

---
+++ rfc_viewer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#FEATURES
+
+#INSTALAÇÃO:
+    #Na instalação/primeira execução do script será buscado o caminho do shell padrão para substituir #!/bin/bash, conforme orientado pelo manual do sh.
+
+#EXECUÇÃO:
+    #As informações serão buscadas do site https://www.rfc-editor.org/
+    #As opções de output serão baseadas nas opções do próprio site, mas opções de exportação em arquivo.
